dc02399ac562773566bbe199a3af92fa5284e968
Ahanaf Akif Pathan <34394942+akifpathan@users.noreply.github.com>
2020-09-27 18:45:56 +0600

    Update segmentTree.h

--- data structure/segmentTree.h
+++ data structure/segmentTree.h
@@ -7,14 +7,22 @@ struct seg_tree
 	seg_tree(int n)
 	{
 		ara.resize(n+1);
-		tree.resize(4*n+1);
+		Tree.resize(4*n+1);
+	}
+	
+	auto func(auto a,auto b)
+	{
+		//for sum: return a+b
+		//for min: return min(a,b)
+		//for max: return max(a,b)
+		
 	}
 	
 	void build(int l,int r,int at)
 	{
 		if(l==r)
 		{
-			tree[at]=ara[l];
+			Tree[at]=ara[l];
 			return;
 		}
 		
@@ -23,49 +31,37 @@ struct seg_tree
 		build(l,mid,at*2);
 		build(mid+1,r,at*2+1);
 		
-		tree[at]=tree[at*2]+tree[at*2+1];
-		//tree[at]=min(tree[at*2],tree[at*2+1]);
-		//tree[at]=max(tree[at*2],tree[at*2]+1);
+		Tree[at]=func(Tree[at*2],Tree[at*2+1]);
 	}
 	
-	int query_sum(int tL,int tR,int rL,int rR,int at)
+	//makes ara[i]=val by default, for increment use Tree[at]+=val
+	void update(int l,int r,int indx,int val,int at)
+	{
+		if(l==r)
+		{
+			Tree[at]=val;
+			return;
+		}
+		
+		int mid=(l+r)/2;
+		
+		if(indx<=mid) update(l,mid,indx,val,at*2);
+		else update(mid+1,r,indx,val,at*2+1);
+		
+		Tree[at]=func(Tree[at*2],Tree[at*2+1]);
+	}
+	
+	int query(int tL,int tR,int rL,int rR,int at)
 	{
 		if(tL>rR || tR<rL) return 0;
-		if(tL>=rL && tR<=rR) return tree[at];
+		if(tL>=rL && tR<=rR) return Tree[at];
 		
 		int mid=(tL+tR)/2;
 		
-		int a=query_sum(tL,mid,rL,rR,at*2);
-		int b=query_sum(mid+1,tR,rL,rR,at*2+1);
+		int a=query(tL,mid,rL,rR,at*2);
+		int b=query(mid+1,tR,rL,rR,at*2+1);
 		
-		return a+b;
-	}
-	
-	int query_max(int tL,int tR,int rL,int rR,int at)
-	{
-		if(tL>rR || tR<rL) return -1000000000;
-		if(tL>=rL && tR<=rR) return tree[at];
-		
-		int mid=(tL+tR)/2;
-		
-		int a=query_max(tL,mid,rL,rR,at*2);
-		int b=query_max(mid+1,tR,rL,rR,at*2+1);
-		
-		return max(a,b);
-	}
-	
-	int query_min(int tL,int tR,int rL,int rR,int at)
-	{
-		if(tL>rR || tR<rL) return 1000000000;
-		if(tL>=rL && tR<=rR) return tree[at];
-		
-		int mid=(tL+tR)/2;
-		
-		int a=query_min(tL,mid,rL,rR,at*2);
-		int b=query_min(mid+1,tR,rL,rR,at*2+1);
-		
-		return a+b;
+		return func(a,b);
 	}
 	
 };
-	
